472eabfab00475ee4be9f55307e2b42d3acab3e1
Psyzoo <patrickmorrier@gmail.com>
2016-10-08 17:57:51 -0400

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+console.log("Hey");
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+
+}
+
